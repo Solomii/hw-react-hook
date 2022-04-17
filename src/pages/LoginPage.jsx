c8f9f18dd0a68bbb86b7c402815dc975d972f760
Solomii <kushniryksolomiya@gmail.com>
2022-04-17 17:47:36 +0300

    [#0] Add hook useRef to input

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 
@@ -6,15 +6,23 @@ export const LoginPage = () => {
   const [user, setUser] = useState("");
   const auth = useAuth();
   const navigate = useNavigate();
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus()
+  },[])
 
   const handleLogin = () => {
-    auth.login(user)
+    auth.login(user);
     navigate('/profile')
   }
 
   return (
     <div className="login_page">
-      <input type="text" placeholder="Enter name..." onChange={(e) => setUser(e.target.value)} />
+      <input ref={inputRef} type="text" placeholder="Enter name..." onChange={(e) => setUser(e.target.value)} />
+      {/* <br />
+      <input type="text" placeholder="Password..." />
+      <br /> */}
       <button onClick={handleLogin}>Login</button>
     </div>
   )
